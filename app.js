11c059efebc4055a3e4fdf54a38aa47921311845
Julio <youremail@example.com>
2024-08-29 16:09:28 -0400

    added form required

--- app.js
+++ app.js
@@ -1,21 +1,22 @@
-// (() => {
-    // 'use strict'
-  
-    // Fetch all the forms we want to apply custom Bootstrap validation styles to
-    // const forms = document.querySelectorAll('.needs-validation')
-  
-    // Loop over them and prevent submission
-  //   Array.from(forms).forEach(form => {
-  //     form.addEventListener('submit', event => {
-  //       if (!form.checkValidity()) {
-  //         event.preventDefault()
-  //         event.stopPropagation()
-  //       }
-  
-  //       form.classList.add('was-validated')
-  //     }, false)
-  //   })
-  // })()
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+(() => {
+  'use strict'
+
+  // Fetch all the forms we want to apply custom Bootstrap validation styles to
+  const forms = document.querySelectorAll('.needs-validation')
+
+  // Loop over them and prevent submission
+  Array.from(forms).forEach(form => {
+    form.addEventListener('submit', event => {
+      if (!form.checkValidity()) {
+        event.preventDefault()
+        event.stopPropagation()
+      }
+
+      form.classList.add('was-validated')
+    }, false)
+  })
+})()
 
   const navBar = document.getElementById('nav-bar');
   const navFun = document.getElementById('list-nav');
